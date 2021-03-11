5d0e3b63dbb588c21a2f32084fb17ee9775e790b
azoritihie <butterfly140611@gmail.com>
2021-03-11 20:04:39 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let result = ''; 
+    if (n < 0) {
+      n = n*(-1);
+    while (n > 0) {
+    result = result + (n % 10);
+    n = parseInt(n/10);
+    }
+    return result;}
+    else {
+      while (n > 0) {
+    result = result + (n % 10);
+    n = parseInt(n/10);
+    }
+    return result;}
+    
+    }
